aa75140523cb3a95eb4771b8cfd915ff2804daec
Reginald Wong <reginald_wong@outlook.com>
2022-05-30 10:22:20 -0400

    add price and image to campground model and reseed data

--- seeds/index.js
+++ seeds/index.js
@@ -17,9 +17,14 @@ const seedDB = async () => {
   await Campground.deleteMany({});
   for (let i = 0; i < 50; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
+      image: 'https://source.unsplash.com/collection/483251',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit enim commodi omnis non expedita velit, incidunt facere possimus cum quia maiores sint atque libero eum amet, officiis eaque tenetur ipsam!',
+      price,
     });
     await camp.save();
   }
