8efddc6c4daef6f9e29b1e70b629a911ab80e240
Reginald Wong <reginald_wong@outlook.com>
2022-06-14 15:37:11 -0400

    create flash success partials

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -65,6 +65,7 @@ router.put(
     const campground = await Campground.findByIdAndUpdate(id, {
       ...req.body.campground,
     });
+    req.flash('success', 'Successfully updated campground!');
     res.redirect(`/campgrounds/${campground._id}`);
   })
 );
@@ -72,6 +73,7 @@ router.put(
 router.delete('/:id', async (req, res) => {
   const { id } = req.params;
   await Campground.findByIdAndDelete(id);
+  req.flash('success', 'Successfully deleted campground!');
   res.redirect('/campgrounds');
 });
 
