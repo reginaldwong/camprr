e991de4106bb2dce0a897d14fb53bbba9b49c9b9
Reginald Wong <reginald_wong@outlook.com>
2022-06-21 09:02:09 -0400

    add returnTo behaviour

--- routes/users.js
+++ routes/users.js
@@ -36,10 +36,13 @@ router.post(
   passport.authenticate('local', {
     failureFlash: true,
     failureRedirect: '/login',
+    keepSessionInfo: true,
   }),
   (req, res) => {
     req.flash('success', 'Welcome back!');
-    res.redirect('/campgrounds');
+    const redirectUrl = req.session.returnTo || '/campgrounds';
+    delete req.session.returnTo;
+    res.redirect(redirectUrl);
   }
 );
 
