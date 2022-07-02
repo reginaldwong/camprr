ed8786685f3e67368c185f3a14b23b98b5b863a8
Reginald Wong <reginald_wong@outlook.com>
2022-07-02 14:21:43 -0400

    deploy

--- app.js
+++ app.js
@@ -23,7 +23,8 @@ const reviewRoutes = require('./routes/reviews');
 
 const MongoStore = require('connect-mongo');
 
-const dbUrl = 'mongodb://localhost:27017/yelp-camp';
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
+
 mongoose.connect(dbUrl);
 
 const db = mongoose.connection;
@@ -43,9 +44,11 @@ app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(mongoSanitize());
 
+const secret = process.env.SECRET || 'thisshouldbeabettersecret';
+
 const store = MongoStore.create({
   mongoUrl: dbUrl,
-  secret: 'thisshouldbeabettersecret',
+  secret,
   touchAfter: 24 * 60 * 60,
 });
 
@@ -56,7 +59,7 @@ store.on('error', function (e) {
 const sessionConfig = {
   store,
   name: 'session',
-  secret: 'thisshouldbeabettersecret!',
+  secret,
   resave: false,
   saveUninitialized: true,
   cookie: {
